0646ff9789728bf446337eaf6da0e3880a82d0fa
roy ledford <bluesixty@gmail.com>
2018-02-26 12:35:47 -0500

    Prep for deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Home from './home/HomeContainer'
-import './App.css'
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
 import './styles/normalize.css'
 import './styles/reset.css'
 import './styles/vars.css'
 import './styles/base.css'
 
-import registerServiceWorker from './registerServiceWorker'
-
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
